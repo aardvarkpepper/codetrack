83012038809f618f46690778a0b8214c1e0924e5
James Lee <aardvarkpepper@gmail.com>
2023-12-01 05:20:36 -0500

    Add leetcode 1021

---
+++ leetcode/p1021.js
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/remove-outermost-parentheses/
+
+var removeOuterParentheses = function(s) {
+  const decomposedArray = [];
+  let openLeftP = 0;
+  for (const character of s) {
+      if (character === "(") {
+          openLeftP++;
+      };
+      if (character === ")") {
+          openLeftP--;
+      }
+      if (openLeftP === 1 && character === "(") {
+          continue;
+      };
+      if (openLeftP === 0 && character === ")") {
+          continue;
+      };
+      decomposedArray.push(character);
+  }
+  return decomposedArray.join("");
+};
+
+/*
+Summed up better by
+var removeOuterParentheses = function(s) {
+
+  let str=''
+  let count=0
+
+  for(let i=0;i<s.length;i++){
+    if(s[i]=='('){
+      if(count>0)
+          str+=s[i]
+    count++
+    }
+    else{
+    count--
+        if(count>0)
+          str+=s[i]
+    } 
+  }
+
+  return str
+    
+};
+*/
