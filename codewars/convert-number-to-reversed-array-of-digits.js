1abb2a01d5e29f846a4143e7862af43187922939
James Lee <aardvarkpepper@gmail.com>
2023-12-05 10:35:33 -0500

    Add codewars Convert number to reversed array of digits.

---
+++ codewars/convert-number-to-reversed-array-of-digits.js
@@ -0,0 +1,14 @@
+/**
+ * 2024 Dec 5
+ * https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/javascript
+ */
+
+function digitize(n) {
+  const nString = String(n);
+  const reversedArray = [];
+  for (let i = nString.length-1; i >=0; i--) {
+    reversedArray.push(Number(nString[i]));
+  }
+  //code here
+  return reversedArray;
+}
