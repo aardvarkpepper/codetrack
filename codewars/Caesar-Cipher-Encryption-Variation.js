9d3755b24cff1e18d8a84210a40d1fe77ef05fcd
James Lee <aardvarkpepper@gmail.com>
2023-12-06 06:09:48 -0500

    Caesar Cipher Encryption - Variation.

---
+++ codewars/Caesar-Cipher-Encryption-Variation.js
@@ -0,0 +1,21 @@
+/**
+ * 2024 Dec 6
+ * https://www.codewars.com/kata/55ec55323c89fc5fbd000019/train/javascript
+ */
+
+function caesarEncode(phrase, shift) {
+  const adjust = "a".charCodeAt(0);
+  const phraseSplit = phrase.split(" ");
+  const encryptedArray = [];
+  for (const word of phraseSplit) {
+    const wordSplit = word.split("");
+    const encryptedWordArray = [];
+    for (const character of wordSplit) {
+      const base26 = (character.charCodeAt(0) - adjust + shift) % 26;
+      encryptedWordArray.push(String.fromCharCode(base26 + adjust))
+    } // for character
+    encryptedArray.push(encryptedWordArray.join(""));
+    shift++;
+  } // for word
+  return encryptedArray.join(" ");
+}
