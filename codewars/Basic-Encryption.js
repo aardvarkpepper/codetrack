4f9545fb00d796d9869296a7e28fade533121a54
James Lee <aardvarkpepper@gmail.com>
2023-12-06 05:43:21 -0500

    Add Codewars Basic Encryption.

---
+++ codewars/Basic-Encryption.js
@@ -0,0 +1,16 @@
+/**
+ * 2024 Dec 6
+ * https://www.codewars.com/kata/5862fb364f7ab46270000078/train/javascript
+ */
+
+function encrypt(text, rule) {
+  //   for (const character of text) {
+  //     console.log(character.charCodeAt(0));
+  //   }
+  //console.log(text, rule);
+  const encryptArray = [];
+  for (const character of text) {
+    encryptArray.push(String.fromCharCode((character.charCodeAt(0) + rule) % 256))
+  }
+  return encryptArray.join("");
+};
