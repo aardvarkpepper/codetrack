cbd9b5adff47afaa66137c3e0348ef51b58cc804
James Lee <aardvarkpepper@gmail.com>
2023-12-08 12:32:04 -0500

    Add Codewars 596c6eb85b0f515834000049.

---
+++ codewars/596c6eb85b0f515834000049.js
@@ -0,0 +1,9 @@
+/**
+ * 2023 Dec 8
+ * https://www.codewars.com/kata/596c6eb85b0f515834000049/train/javascript
+ * FIXME: Replace all dots
+ */
+
+var replaceDots = function(str) {
+  return str.replaceAll(".", '-');
+}
