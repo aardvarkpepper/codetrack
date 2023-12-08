8234b359c8c1f73bfc74e30e398fa86f74a6c4a2
James Lee <aardvarkpepper@gmail.com>
2023-12-08 12:26:57 -0500

    Add Codewars 56170e844da7c6f647000063.

---
+++ codewars/56170e844da7c6f647000063.js
@@ -0,0 +1,18 @@
+/**
+ * 2023 Dec 8
+ * https://www.codewars.com/kata/56170e844da7c6f647000063/train/javascript
+ * Drink about
+ */
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return "drink toddy";
+  }
+  if (old < 18) {
+    return "drink coke";
+  }
+  if (old < 21) {
+    return "drink beer";
+  }
+  return "drink whisky"
+};
