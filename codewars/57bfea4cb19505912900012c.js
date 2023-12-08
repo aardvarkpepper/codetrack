9b9d5b06242b32206c83273d80b2d68d52a2acd1
James Lee <aardvarkpepper@gmail.com>
2023-12-08 11:40:56 -0500

    Add Codewars 5834fec22fb0ba7d080000e8.

---
+++ codewars/57bfea4cb19505912900012c.js
@@ -0,0 +1,9 @@
+/**
+ * 2023 Dec 8
+ * https://www.codewars.com/kata/57bfea4cb19505912900012c/train/javascript
+ * Points of Reflection
+ */
+
+function symmetricPoint(p, q) {
+  return [q[0] + (q[0] - p[0]), q[1] + (q[1] - p[1])]
+}
