d6479ef820e26601616b72d365cc6972e79c112f
James Lee <aardvarkpepper@gmail.com>
2023-12-08 11:59:02 -0500

    Add Codewars 55dc4520094bbaf50e0000cb.

---
+++ codewars/55dc4520094bbaf50e0000cb.js
@@ -0,0 +1,9 @@
+/**
+ * 2023 Dec 8
+ * https://www.codewars.com/kata/55dc4520094bbaf50e0000cb/train/javascript
+ * Wilson primes
+ */
+
+function amIWilson(p) {
+  return p === 5 || p === 13 || p === 563
+}
