94238255a64f87d2f11c39510694e2910f380203
James Lee <aardvarkpepper@gmail.com>
2023-12-08 09:21:54 -0500

    Add Codewars Function 1 - hello world.

---
+++ codewars/Function-1-hello-world.js
@@ -0,0 +1,8 @@
+/**
+ * 2024 Dec 8
+ * https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+ */
+
+const greet = () => {
+  return "hello world!"
+  }// Write a function "greet" that returns "hello world!"
