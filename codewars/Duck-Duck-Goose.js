affc2f4840edd4722ca433bc8bb6be36ac36d506
James Lee <aardvarkpepper@gmail.com>
2023-12-08 09:47:36 -0500

    Add Codewars Duck Duck Goose.

---
+++ codewars/Duck-Duck-Goose.js
@@ -0,0 +1,12 @@
+/**
+ * 2023 Dec 8
+ * https://www.codewars.com/kata/582e0e592029ea10530009ce/train/javascript
+ */
+
+function duckDuckGoose(players, goose) {
+  while (goose > players.length) {
+    goose = goose - players.length;
+  }
+  return players[goose - 1].name;
+  // ...
+}
