76563acbb6073afcea5c6dd03b599f2b10c07fa3
James Lee <aardvarkpepper@gmail.com>
2023-12-08 09:34:27 -0500

    Add Codewars Convert a String to a Number

---
+++ codewars/Convert-a-String-to-a-Number!.js
@@ -0,0 +1,9 @@
+/**
+ * 2024 Dec 8
+ * https://www.codewars.com/kata/544675c6f971f7399a000e79/javascript
+ */
+
+const stringToNumber = function(str){
+  // put your code here
+  return Number(str);
+}
