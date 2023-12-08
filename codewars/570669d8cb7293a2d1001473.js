abc97bfa93ca6a81a9275afbb7a6e9e033499b80
James Lee <aardvarkpepper@gmail.com>
2023-12-08 10:05:53 -0500

    Add Codewars 570669d8cb7293a2d1001473.

---
+++ codewars/570669d8cb7293a2d1001473.js
@@ -0,0 +1,9 @@
+/**
+ * 2023 Dec 8
+ * https://www.codewars.com/kata/570669d8cb7293a2d1001473/train/javascript
+ * Chuck Norris VII - True or False? (Beginner)
+ */
+
+function ifChuckSaysSo(){
+  return !true;
+}
