135cea6c2cc601a7da8d90a1ae6c5ae62bf8f722
James Lee <aardvarkpepper@gmail.com>
2023-12-09 07:35:37 -0500

    Add Codewars 5899dc03bc95b1bf1b0000ad.

---
+++ codewars/5899dc03bc95b1bf1b0000ad.js
@@ -0,0 +1,9 @@
+/**
+ * 2023 Dec 9
+ * https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/javascript
+ * Invert values
+ */
+
+function invert(array) {
+  return array.map(element => -element)
+}
