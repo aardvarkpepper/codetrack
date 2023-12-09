30cfa44b02b2d297c1b925395784cbc1894a628e
James Lee <aardvarkpepper@gmail.com>
2023-12-09 08:23:28 -0500

    Add Codewars 56dec885c54a926dcd001095.

---
+++ codewars/56dec885c54a926dcd001095.js
@@ -0,0 +1,10 @@
+/**
+ * 2023 Dec 9
+ * https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+ * Opposite number
+ */
+
+function opposite(number) {
+  return -number;
+  //your code here
+}
