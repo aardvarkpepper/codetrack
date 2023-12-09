63bf927d7d3312739d1cf737f94646665b6ca4a0
James Lee <aardvarkpepper@gmail.com>
2023-12-09 08:26:39 -0500

    Add Codewars 57356c55867b9b7a60000bd7.

---
+++ codewars/57356c55867b9b7a60000bd7.js
@@ -0,0 +1,22 @@
+/**
+ * 2023 Dec 9
+ * https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+ * Basic Mathematical Operations
+ */
+
+function basicOp(operation, value1, value2)
+{
+  switch (operation) {
+      case ("+"):
+      return value1 + value2;
+      case ("-"):
+      return value1 - value2;
+      case ("*"):
+      return value1 * value2;
+      case ("/"):
+      return value1 / value2;
+      default:
+      return "Operation not found";
+  }
+  // Code
+}
