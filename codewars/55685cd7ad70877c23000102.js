ce15159d38877e9d086c86ec762b7664ca4c1ced
James Lee <aardvarkpepper@gmail.com>
2023-12-09 08:40:00 -0500

    Add Codewars 55685cd7ad70877c23000102.

---
+++ codewars/55685cd7ad70877c23000102.js
@@ -0,0 +1,10 @@
+/**
+ * 2023 Dec 9
+ * https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+ * Return Negative
+ */
+
+function makeNegative(num) {
+  return num < 0 ? num : -num
+  // Code?
+}
