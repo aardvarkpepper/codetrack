7baf2a8076b829b49e9bb5150daac3b6c6dadde0
James Lee <aardvarkpepper@gmail.com>
2023-12-09 07:27:39 -0500

    Add Codewars 5168bb5dfe9a00b126000018.

---
+++ codewars/5168bb5dfe9a00b126000018.js
@@ -0,0 +1,9 @@
+/**
+ * 2023 Dec 9
+ * https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+ * Reversed Strings
+ */
+
+function solution(str){
+  return str.split('').reverse().join('');  
+}
