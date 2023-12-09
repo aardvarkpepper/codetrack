67f9250ec866d1162fa6b77ad461589c1111b305
James Lee <aardvarkpepper@gmail.com>
2023-12-09 08:38:37 -0500

    Add Codewars 551b4501ac0447318f0009cd.

---
+++ codewars/551b4501ac0447318f0009cd.js
@@ -0,0 +1,10 @@
+/**
+ * 2023 Dec 9
+ * https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+ * Convert a Boolean to a String
+ */
+
+function booleanToString(b){
+  return b ? "true" : "false";
+  //your code here
+}
