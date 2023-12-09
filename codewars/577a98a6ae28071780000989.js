ec89c64e39c895bc999764469bdec2951de8ccb9
James Lee <aardvarkpepper@gmail.com>
2023-12-09 07:47:54 -0500

    Add Codewars 577a98a6ae28071780000989.

--- codewars/577a98a6ae28071780000989.js
+++ codewars/577a98a6ae28071780000989.js
@@ -4,22 +4,22 @@
  * Find Maximum and Minimum Values of a List
  */
 
-var min = function(list){
+var min = function (list) {
   let min = list[0];
   for (let i = 1; i < list.length; i++) {
     if (list[i] < min) {
       min = list[i]
     }
   }
-    return list[i];
+  return min;
 }
 
-var max = function(list){
+var max = function (list) {
   let max = list[0];
   for (let i = 1; i < list.length; i++) {
     if (list[i] > max) {
       max = list[i]
     }
   }
-    return list[i];
+  return max;
 }
