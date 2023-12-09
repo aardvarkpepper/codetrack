ac121e3b81fb53fd577e330f9eecffd80e69c4b1
James Lee <aardvarkpepper@gmail.com>
2023-12-09 08:53:55 -0500

    Add Codewars 5a3fe3dde1ce0e8ed6000097.

---
+++ codewars/5a3fe3dde1ce0e8ed6000097.js
@@ -0,0 +1,10 @@
+/**
+ * 2023 Dec 9
+ * https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+ * Century From Year
+ */
+
+function century(year) {
+  // Finish this :)
+  return Math.ceil(year/100);
+}
