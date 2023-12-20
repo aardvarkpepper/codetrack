df85358c1ef4bdba83cdcbbbfe0342aabd89ce89
James Lee <aardvarkpepper@gmail.com>
2023-12-20 10:47:06 -0500

    Add 2023 Dec 20 second class problem.

--- classProblems/2023_12_20.js
+++ classProblems/2023_12_20.js
@@ -1,7 +1,17 @@
 /**
+ * 2023 Dec 20
  * https://the-winter.github.io/codingjs/exercise.html?name=sleepIn&title=Warmup-1
  */
 
 function sleepIn(weekday, vacation){
   return (!weekday || vacation)
+}
+
+/**
+ * https://the-winter.github.io/codingjs/exercise.html?name=doubleX&title=Warmup-2
+ */
+
+function doubleX(str){
+  const firstX = str.indexOf("x");
+  return str[firstX+1] === "x";
 }
