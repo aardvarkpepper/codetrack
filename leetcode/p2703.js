ea286f5a61ae2ef82f1e54a86fbdd7c1dc03cce8
James Lee <aardvarkpepper@gmail.com>
2023-12-19 23:21:27 -0500

    Add Leetcode 2703.

---
+++ leetcode/p2703.js
@@ -0,0 +1,10 @@
+/**
+ * 2023 Dec 19
+ * https://leetcode.com/problems/return-length-of-arguments-passed/?envType=study-plan-v2&envId=30-days-of-javascript
+ * Return Length of Arguments Passed
+ */
+
+var argumentsLength = function(...args) {
+  const lengthMe = [...args];
+  return lengthMe.length;
+};
