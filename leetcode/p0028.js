a16a50b82e67256259f1f942f42497aff30cf45a
James Lee <aardvarkpepper@gmail.com>
2023-12-21 04:53:18 -0500

    Add Leetcode 28.

---
+++ leetcode/p0028.js
@@ -0,0 +1,9 @@
+/**
+ * 2023 Dec 21
+ * https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+ * Find the Index of the First Occurrence in a String
+ */
+
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle)
+};
