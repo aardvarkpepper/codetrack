a7ccb5572ced765f9d3f237b13fd90282729a5ff
James Lee <aardvarkpepper@gmail.com>
2023-12-23 05:00:41 -0500

    Add corrected code to Leetcode 1260.

--- leetcode/p1260.js
+++ leetcode/p1260.js
@@ -5,24 +5,26 @@
  */
 
 var shiftGrid = function(grid, k) {
-  console.log("grid", grid, "k", k)
-  let rows = grid.length;
-  let columns = grid[0].length;
-  let matrixLength = rows * columns;
-  const matrixShifted = Array.from(Array(rows), () => Array(columns));
-  const convertIndexToMatrix = (index2d) => {
-      return [Math.floor(index2d / rows), index2d % columns]
-  }
-  // let's see if it lets us get away with non-simultaneous assignment
-  for (i = 0; i < matrixLength; i++) {
-      let shiftedIndex = i+k;
-      while (shiftedIndex >= matrixLength) {
-          shiftedIndex -= matrixLength;
-      }
-      console.log("i", i, "shiftI", shiftedIndex);
-      const [yIndex, xIndex] = convertIndexToMatrix(i)
-      const [yIndexShifted, xIndexShifted] = convertIndexToMatrix(shiftedIndex)
-      matrixShifted[yIndexShifted][xIndexShifted] = grid[yIndex][xIndex]
-  }
-  return matrixShifted;
+    let rows = grid.length;
+    let columns = grid[0].length;
+    let matrixLength = rows * columns;
+    //console.log("grid", grid, "k", k, "rows", rows, "columns", columns)
+    const matrixShifted = Array.from(Array(rows), () => Array(columns));
+    const convertIndexToMatrix = (index2d) => {
+        return [Math.floor(index2d / columns), index2d % columns]
+    }
+    // let's see if it lets us get away with non-simultaneous assignment
+    for (i = 0; i < matrixLength; i++) {
+        let shiftedIndex = i+k;
+        while (shiftedIndex >= matrixLength) {
+            shiftedIndex -= matrixLength;
+        }
+        //console.log("i", i, "shiftI", shiftedIndex);
+        const [yIndex, xIndex] = convertIndexToMatrix(i)
+        const [yIndexShifted, xIndexShifted] = convertIndexToMatrix(shiftedIndex)
+        //console.log("yIndex", yIndex, "xIndex", xIndex);
+        //console.log("yIndexShifted", yIndexShifted, "xIndexShifted", xIndexShifted);
+        matrixShifted[yIndexShifted][xIndexShifted] = grid[yIndex][xIndex]
+    }
+    return matrixShifted;
 };
