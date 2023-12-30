9f0e0cecffadfa9fda11e7e96ea41a790ea1554d
James Lee <aardvarkpepper@gmail.com>
2023-12-30 14:12:02 -0500

    Add Codewars 557cd6882bfa3c8a9f0000c1.

---
+++ codewars/557cd6882bfa3c8a9f0000c1.js
@@ -0,0 +1,10 @@
+/**
+ * 2023 Dec 30
+ * https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/javascript
+ * Parse nice int from char problem
+ */
+
+function getAge(inputString){
+  return Number(inputString[0]);
+// return the girl's correct age as an integer. Happy coding :) 
+}
