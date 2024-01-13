e3c5118f6700dedbe42169cfe09c54f923accd95
James Lee <aardvarkpepper@gmail.com>
2024-01-13 02:25:50 -0500

    Add Codewars 583710ccaa6717322c000105.

---
+++ codewars/5556282156230d0e5e000089.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 10
+ * https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
+ * DNA to RNA Conversion
+ */
+
+function DNAtoRNA(dna) {
+  return dna.replaceAll("T", "U");
+  // create a function which returns an RNA sequence from the given DNA sequence
+}
---
+++ codewars/583710ccaa6717322c000105.js
@@ -0,0 +1,13 @@
+/**
+ * 2024 Jan 13
+ * https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+ * Simple multiplication
+ */
+
+function simpleMultiplication(number) {
+  if (number % 2 === 0) {
+    return number * 8
+  }
+  return number * 9
+    // your code........
+}
