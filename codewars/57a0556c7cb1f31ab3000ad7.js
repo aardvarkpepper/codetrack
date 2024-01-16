cee737cd46dbb9bda6fadb05096b407e66a4177c
James Lee <aardvarkpepper@gmail.com>
2024-01-16 18:40:38 -0500

    Add Codewars 57a0556c7cb1f31ab3000ad7.

---
+++ codewars/57a0556c7cb1f31ab3000ad7.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 16
+ * https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+ * MakeUpperCase
+ */
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+  // Code here
+}
