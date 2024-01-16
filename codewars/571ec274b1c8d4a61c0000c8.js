d70170c03bd5fdd83e13cd1ebf5a4b7aba85bbcc
James Lee <aardvarkpepper@gmail.com>
2024-01-16 18:38:04 -0500

    Add Codewars 571ec274b1c8d4a61c0000c8.

---
+++ codewars/571ec274b1c8d4a61c0000c8.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 16
+ * https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+ * Training JS #1: create your first JS function and print "Hello World!"
+ */
+
+function helloWorld () {
+  let str = "Hello World!"
+  console.log(str)
+}
