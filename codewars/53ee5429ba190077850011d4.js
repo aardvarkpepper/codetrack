d49fca181e7037a77d30695c5b4150788646f748
James Lee <aardvarkpepper@gmail.com>
2024-01-16 18:35:33 -0500

    Add Codewars 53ee5429ba190077850011d4.

---
+++ codewars/53ee5429ba190077850011d4.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 16
+ * https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+ * You Can't Code Under Pressure #1
+ */
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return 2 * i;
+}
