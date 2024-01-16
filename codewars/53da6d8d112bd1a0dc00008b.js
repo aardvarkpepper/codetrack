89450c168dd567816fad407bd5b9220b95432a07
James Lee <aardvarkpepper@gmail.com>
2024-01-16 18:34:24 -0500

    Add Codewars 53da6d8d112bd1a0dc00008b.

---
+++ codewars/53da6d8d112bd1a0dc00008b.js
@@ -0,0 +1,9 @@
+/**
+ * 2024 Jan 16
+ * https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/train/javascript
+ * Reverse List Order
+ */
+
+function reverseList(list) {
+  return list.reverse();
+}
