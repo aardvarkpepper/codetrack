7ba1686b7d18c4252833ebdf3679572387992e94
James Lee <aardvarkpepper@gmail.com>
2024-01-21 02:48:45 -0500

    Add Codewars 55cb632c1a5d7b3ad0000145.

---
+++ codewars/55cb632c1a5d7b3ad0000145.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 21
+ * https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
+ * Keep up the hoop
+ */
+
+function hoopCount (n) {
+  return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+   //your code goes here    
+}
