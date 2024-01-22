3d95c88cb000211843ce22cd8478047891d749de
James Lee <aardvarkpepper@gmail.com>
2024-01-22 15:51:49 -0500

    Add Codewars 53af2b8861023f1d88000832.

---
+++ codewars/53af2b8861023f1d88000832.js
@@ -0,0 +1,13 @@
+/**
+ * 2024 Jan 22
+ * https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+ * Are You Playing Banjo?
+ */
+
+function areYouPlayingBanjo(name) {
+  if (name[0].toLowerCase() === "r") {
+    return `${name} plays banjo`
+  }
+  // Implement me
+  return `${name} does not play banjo`
+}
