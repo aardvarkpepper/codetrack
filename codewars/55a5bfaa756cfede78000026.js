3637e7a89a4685f3fd777b22af3e102cb86d7ea6
James Lee <aardvarkpepper@gmail.com>
2024-01-22 15:31:27 -0500

    Add Codewars 55a5bfaa756cfede78000026.

---
+++ codewars/55a5bfaa756cfede78000026.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 22
+ * https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/javascript
+ * Super Duper Easy
+ */
+
+function problem(x){
+  return typeof(x) === "number" ? 50 * x + 6 : "Error"
+  //your code here
+}
