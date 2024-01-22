5a82c65daacb0dbcf9ac46afd26f5706cad79cfa
James Lee <aardvarkpepper@gmail.com>
2024-01-22 15:53:18 -0500

    Add Codewars 55f9bca8ecaa9eac7100004a.

---
+++ codewars/55f9bca8ecaa9eac7100004a.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 22
+ * https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+ * Beginner Series #2 Clock
+ */
+
+function past(h, m, s){
+  return s*1000 + m*60000 + h*3600000
+  //#Happy Coding! ^_^
+}
