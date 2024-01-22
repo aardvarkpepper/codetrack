4fff43a80c5d0aaca73df6e8ac03eb5105c5656a
James Lee <aardvarkpepper@gmail.com>
2024-01-22 15:33:23 -0500

    Add Codewars 55ecd718f46fba02e5000029.

---
+++ codewars/55ecd718f46fba02e5000029.js
@@ -0,0 +1,14 @@
+/**
+ * 2024 Jan 22
+ * https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/javascript
+ * What is between?
+ */
+
+function between(a, b) {
+  const returnArray = [];
+  for (let i = a; i <= b; i++) {
+    returnArray.push(i);
+  }
+  return returnArray;
+  // your code here
+}
