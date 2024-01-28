a4df6d28cc44b41aec462f4200fa8e175520dde9
James Lee <aardvarkpepper@gmail.com>
2024-01-28 05:24:23 -0500

    Add Codewars 523b623152af8a30c6000027.

---
+++ codewars/523b623152af8a30c6000027.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 28
+ * https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+ * Function 2 - squaring an argument
+ */
+
+// Write the "square"-function here
+const square = (inputNumber) => {
+  return inputNumber ** 2
+}
