a619789ce7556441e9cc2d362c73ff9c376f3d5e
James Lee <aardvarkpepper@gmail.com>
2024-01-29 00:15:00 -0500

    Add Codewars 57a55c8b72292d057b000594.

---
+++ codewars/57a55c8b72292d057b000594.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 29
+ * https://www.codewars.com/kata/57a55c8b72292d057b000594/train/javascript
+ * Reversing Words in a String
+ */
+
+function reverse(string){
+  return string.split(" ").reverse().join(" ");
+  //your code here
+}
