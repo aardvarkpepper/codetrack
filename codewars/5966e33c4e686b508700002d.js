6ba88406e81b1e321183535932423b02fec1fd07
James Lee <aardvarkpepper@gmail.com>
2024-01-29 00:08:04 -0500

    Add Codewars 5966e33c4e686b508700002d.

---
+++ codewars/5966e33c4e686b508700002d.js
@@ -0,0 +1,9 @@
+/**
+ * 2024 Jan 29
+ * https://www.codewars.com/kata/5966e33c4e686b508700002d/solutions/javascript
+ * Sum The Strings
+ */
+
+function sumStr(a,b) {
+  return String(Number(a) + Number(b))
+}
