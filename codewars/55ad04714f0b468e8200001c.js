06d2cb735b8d32844cab1f11ee769a21fea7df54
James Lee <aardvarkpepper@gmail.com>
2024-01-29 00:31:51 -0500

    Add Codewars 55ad04714f0b468e8200001c.

---
+++ codewars/55ad04714f0b468e8200001c.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Jan 29
+ * https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/javascript
+ * get character from ASCII Value
+ */
+
+function getChar(c){
+  return String.fromCharCode(c);
+  // ...
+}
