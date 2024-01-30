99e821833a9f296fd59f869c7712c0241c950157
James Lee <aardvarkpepper@gmail.com>
2024-01-30 04:51:57 -0500

    Add Codewars 51f9d93b4095e0a7200001b8.

---
+++ codewars/51f9d93b4095e0a7200001b8.js
@@ -0,0 +1,9 @@
+/**
+ * 2024 Jan 30
+ * https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/train/javascript
+ * How many lightsabers do you own?
+ */
+
+function howManyLightsabersDoYouOwn(name) {
+  return name === "Zach" ? 18 : 0
+}
