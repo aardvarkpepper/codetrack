92643eead7ebdd72ae863c1195709442364b1db2
James Lee <aardvarkpepper@gmail.com>
2024-02-01 04:41:59 -0500

    Add Codewars 51f2b4448cadf20ed0000386.

---
+++ codewars/51f2b4448cadf20ed0000386.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Feb 1
+ * https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/javascript
+ * Remove anchor from URL
+ */
+
+function removeUrlAnchor(url){
+  return url.split("#")[0]
+  // TODO: complete
+}
