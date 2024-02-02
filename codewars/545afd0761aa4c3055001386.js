3a66d152f08b9357081f1abd951ec3eb1f389f05
James Lee <aardvarkpepper@gmail.com>
2024-02-02 06:39:13 -0500

    Add Codewars 545afd0761aa4c3055001386.

---
+++ codewars/545afd0761aa4c3055001386.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Feb 2
+ * https://www.codewars.com/kata/545afd0761aa4c3055001386/train/javascript
+ * Enumerable Magic #25 - Take the First N Elements
+ */
+
+function take(arr, n) {
+  return arr.slice(0,n);
+  // Your code here
+}
