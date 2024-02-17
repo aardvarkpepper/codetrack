59ba9bdb4d79057b9b57f3fbf1e28efd80948170
James Lee <aardvarkpepper@gmail.com>
2024-02-17 03:23:48 -0500

    Add Codewars 59fca81a5712f9fa4700159a.

---
+++ codewars/59fca81a5712f9fa4700159a.js
@@ -0,0 +1,9 @@
+/**
+ * 2024 Feb 17
+ * https://www.codewars.com/kata/59fca81a5712f9fa4700159a/train/javascript
+ * Convert to Binary
+ */
+
+function toBinary(n){
+  return Number(n.toString(2))
+}
