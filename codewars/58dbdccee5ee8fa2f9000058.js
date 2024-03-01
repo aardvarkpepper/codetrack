02e9a226437486c8f4bc45b2785936d2b5da7781
James Lee <aardvarkpepper@gmail.com>
2024-03-01 01:13:47 -0500

    Add Codewars 58dbdccee5ee8fa2f9000058.

---
+++ codewars/58dbdccee5ee8fa2f9000058.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Mar 1
+ * https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
+ * Do you speak "English"?
+ */
+
+function spEng(sentence){
+  return sentence.toLowerCase().includes("english")
+//write your code here
+}
