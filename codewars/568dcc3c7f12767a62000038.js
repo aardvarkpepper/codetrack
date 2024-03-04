62d9a4ac5784b9a82057f70777a5d81e513b3bfb
James Lee <aardvarkpepper@gmail.com>
2024-03-03 20:12:44 -0500

    Add Codewars 568dcc3c7f12767a62000038.

---
+++ codewars/568dcc3c7f12767a62000038.js
@@ -0,0 +1,9 @@
+/**
+ * 2024 Mar 3
+ * https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/javascript
+ * L1: Set Alarm
+ */
+
+function setAlarm(employed, vacation){
+  return (employed && !vacation)
+}
