f6c486be5c3d6e6522de246062786f2d611bfdf0
James Lee <aardvarkpepper@gmail.com>
2024-03-04 09:38:14 -0500

    Add Codewars 51fd6bc82bc150b28e0000ce.

---
+++ codewars/51fd6bc82bc150b28e0000ce.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Mar 4
+ * https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
+ * No oddities here
+ */
+
+function noOdds( values ){
+  return values.filter(element => element % 2 === 0)
+  // Return all non-odd values
+}
