f7a1abc47d7fa566555bad3f1781825d57490558
James Lee <aardvarkpepper@gmail.com>
2024-03-06 15:39:23 -0500

    Add Codewars 5302d846be2a9189af0001e4.

---
+++ codewars/5302d846be2a9189af0001e4.js
@@ -0,0 +1,9 @@
+/**
+ * 2024 Mar 6
+ * https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
+ * Welcome to the City
+ */
+
+function sayHello( name, city, state ) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`
+}
