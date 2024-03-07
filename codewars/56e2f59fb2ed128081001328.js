0bd82a3cbe3d2f1a0d96c8e76ad61d7fad287250
James Lee <aardvarkpepper@gmail.com>
2024-03-07 07:48:15 -0500

    Add Codewars 56e2f59fb2ed128081001328.

---
+++ codewars/56e2f59fb2ed128081001328.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Mar 7
+ * https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+ * Printing Array elements with Comma delimiters
+ */
+
+function printArray(array){
+  return array.join(",")
+  //show me the code!
+}
