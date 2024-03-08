77bc06f9df4ab22c69cc18cc0265dcbe5ab51915
James Lee <aardvarkpepper@gmail.com>
2024-03-08 15:55:24 -0500

    Add Codewars 555a67db74814aa4ee0001b5.

---
+++ codewars/555a67db74814aa4ee0001b5.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Mar 8
+ * https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript
+ * Is it even?
+ */
+
+function testEven(n) {
+  return n % 2 === 0
+    //Your awesome code here!
+}
