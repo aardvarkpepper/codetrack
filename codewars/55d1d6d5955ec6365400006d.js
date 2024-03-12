e4b8d1e8ad425a2c8161628da2b5b7648dd3d088
James Lee <aardvarkpepper@gmail.com>
2024-03-12 19:57:58 -0400

    Add Codewars 569d488d61b812a0f7000015.

---
+++ codewars/55d1d6d5955ec6365400006d.js
@@ -0,0 +1,10 @@
+/**
+ * 2024 Mar 9
+ * https://www.codewars.com/kata/55d1d6d5955ec6365400006d/train/javascript
+ * Round up to the next multiple of 5
+ */
+
+function roundToNext5(n){
+  // ...
+  return Math.ceil(n / 5) * 5
+}
---
+++ codewars/569d488d61b812a0f7000015.js
@@ -0,0 +1,19 @@
+/**
+ * 2024 Mar 12
+ * https://www.codewars.com/kata/569d488d61b812a0f7000015/solutions/javascript
+ * Data Reverse
+ */
+
+function dataReverse(data) {
+  const arrayStor = [];
+  let index = 0;
+  while (index < data.length) {
+    const subArrayStor = [];
+    for (let i = 0; i <= 7; i++) {
+      subArrayStor.push(data[index + i])
+    }
+    arrayStor.push(subArrayStor)
+    index += 8;
+  }
+  return arrayStor.reverse().flat();
+}
